977984d55bdbcf895da6419676550cf3f87ec049
vildan1 <vildanakbulut98@gmail.com>
2023-03-26 00:51:04 +0300

    index review

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 async function getUser(id) {
+  //Axios call to get user data
   const { data: users } = await axios("https://jsonplaceholder.typicode.com/users/" + id);
+  //Axios call to get post data
   const { data: posts } = await axios("https://jsonplaceholder.typicode.com/posts?id=" + id);
   console.log("Users:", users);
   console.log("Posts:", posts);
-
 }
 export default getUser;
